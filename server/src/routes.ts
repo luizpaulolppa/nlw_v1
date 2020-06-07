fda833bbcda02e280808f31df95fe40a0212882f
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-06-06 21:09:56 -0300

    fix scripts node end refactory import

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,19 @@
 import express from 'express';
-import Knex from "knex";
+import Knex from "./database/connection";
 
 const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
   const items = await Knex('items').select('*');
 
-  return response.json(items);
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
